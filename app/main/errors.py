005f66df68e0f9d84fdbe0ad4d10e1cf4bc03275
Tu276 <tu276nathan@gmail.com>
2020-10-01 14:49:10 +0300

    add initial custom error.html file

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,8 +4,10 @@ app = flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template("custom_page.html")
+  return render_template("index.html")
     
 @app.errorhandler(404)
 def not_found(e):
   return render_template('custom_page2.html'), 404
+
+# repaet to handle 403 401 etc................................................
